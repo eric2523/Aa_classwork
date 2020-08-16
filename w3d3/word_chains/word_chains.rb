e14d51d2a4287934302c7a8c4d68bf855d9fbf8d
eric2523@gmail.com <eric2523@gmail.com>
2020-08-16 00:09:06 -0700

    Add incomplete run method

--- w3d3/word_chains/word_chains.rb
+++ w3d3/word_chains/word_chains.rb
@@ -1,8 +1,6 @@
 require "set"
 
 class WordChainer
-    attr_reader :dictionary
-
     def initialize(dictionary_file_name)
         @dictionary = Set.new
         IO.foreach(dictionary_file_name) {|line| @dictionary.add(line.to_s.chomp)}
@@ -22,6 +20,29 @@ class WordChainer
         adj_words
     end
 
+    def run(source, target)
+        #incomplete: what is target??
+        @current_words = [source]
+        @all_seen_words = [source]
+
+        until @current_words.empty?
+            @new_current_words = []
+            @current_words.each do |current_word|
+                self.adjacent_words(current_word).each do |adj_word|
+                    if !@all_seen_words.include?(adj_word)
+                        @new_current_words.push(adj_word)
+                        @all_seen_words.push(adj_word)
+                    end
+                end
+            end
+            puts new_current_words
+            #makes new @current_words to find new adj words 
+            @current_word = @new_current_words
+        end
+    end
+
+    private
+    attr_reader :dictionary
 end
 
 # test = WordChainer.new("dictionary.txt")
