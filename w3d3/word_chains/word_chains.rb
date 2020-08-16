cb08c009e95dbaeba92883bdbfb129ca686e162c
eric2523@gmail.com <eric2523@gmail.com>
2020-08-15 23:39:11 -0700

    Add adjacent_words method Find words that differ from one letter

--- w3d3/word_chains/word_chains.rb
+++ w3d3/word_chains/word_chains.rb
@@ -7,5 +7,22 @@ class WordChainer
         @dictionary = Set.new
         IO.foreach(dictionary_file_name) {|line| @dictionary.add(line.to_s.chomp)}
     end
+
+    def adjacent_words(word)
+        adj_words = []
+        word.each_char.with_index do |char, i|
+            ('a'..'z').each do |letter|
+                next if char == letter 
+                a_word = word.dup
+                a_word[i] = letter
+
+                adj_words.push(a_word) if dictionary.include?(a_word)
+            end
+        end
+        adj_words
+    end
+
 end
 
+# test = WordChainer.new("dictionary.txt")
+# p test.adjacent_words("abet")
