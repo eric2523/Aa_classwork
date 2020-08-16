68cdd45831e639b322bb083032e9d76103eb0652
eric2523@gmail.com <eric2523@gmail.com>
2020-08-15 23:15:44 -0700

    Add dictionary Set

--- w3d3/word_chains/word_chains.rb
+++ w3d3/word_chains/word_chains.rb
@@ -1,3 +1,11 @@
+require "set"
+
 class WordChainer
-    
+    attr_reader :dictionary
+
+    def initialize(dictionary_file_name)
+        @dictionary = Set.new
+        IO.foreach(dictionary_file_name) {|line| @dictionary.add(line.to_s.chomp)}
+    end
 end
+
