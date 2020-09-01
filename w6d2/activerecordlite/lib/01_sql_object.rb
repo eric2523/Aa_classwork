fefc2c0ee54cfed6ae78700bf938bb6f2ceeeaf8
eric2523@gmail.com <eric2523@gmail.com>
2020-09-01 15:14:33 -0700

    Finish Phase1c Getters and Setters

--- w6d2/activerecordlite/lib/01_sql_object.rb
+++ w6d2/activerecordlite/lib/01_sql_object.rb
@@ -1,14 +1,37 @@
 require_relative 'db_connection'
 require 'active_support/inflector'
+require "byebug"
 # NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
 # of this project. It was only a warm up.
 
 class SQLObject
   def self.columns
     # ...
+    return @columns if @columns 
+    result = DBConnection.execute2(<<-SQL).first
+      SELECT 
+        *
+      FROM
+        #{self.table_name}
+    SQL
+    symbols = result.map(&:to_sym)
+    @columns = symbols
   end
 
   def self.finalize!
+    self.columns.each do |column|
+      #getter
+      define_method(column) do 
+        self.attributes[column] unless self.attributes[column].nil?
+      end
+
+      #setter
+      define_method("#{column}=") do |value|
+        # debugger
+        self.attributes[column] = value unless self.attributes[column]
+      end
+
+    end
   end
 
   def self.table_name=(table_name)
@@ -22,14 +45,7 @@ class SQLObject
   end
 
   def self.all
-    # ...
-    # DBConnection.execute(<<-SQL)
-    #   select
-    #     *
-    #   from
-    #     self
-    # SQL
-
+   
   end
 
   def self.parse_all(results)
@@ -42,10 +58,12 @@ class SQLObject
 
   def initialize(params = {})
     # ...
+    
   end
 
   def attributes
     # ...
+    @attributes ||= {}
   end
 
   def attribute_values
