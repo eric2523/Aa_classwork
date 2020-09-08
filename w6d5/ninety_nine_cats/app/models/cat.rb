3bbf5c60e790cee1c52c3e0213c26629d6a8eb7f
eric2523@gmail.com <eric2523@gmail.com>
2020-09-07 19:17:29 -0700

    Create cat model and table

---
+++ w6d5/ninety_nine_cats/app/models/cat.rb
@@ -0,0 +1,16 @@
+class Cat < ApplicationRecord
+    include ActionView::Helpers::DateHelper 
+
+    COLORS = ['Orange', 'White', 'Black', 'Gray']
+
+    validates :birth_date, presence: true 
+    validates :color, presence: true, inclusion: {in: COLORS}
+    validates :sex, presence: true, length: { maximum: 1 }, inclusion: {in: ["M", "F"]}
+    validates :description, presence: true 
+    
+    def age
+        Time.now.year - self.birth_date.year
+    end
+    
+
+end
