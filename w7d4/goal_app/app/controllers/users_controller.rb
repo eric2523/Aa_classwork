a99cff4952ccb10fbf72fd6c0e1374437d39c314
eric2523@gmail.com <eric2523@gmail.com>
2020-09-10 17:08:17 -0700

    Create edit specs and edit and update Users controller

--- w7d4/goal_app/app/controllers/users_controller.rb
+++ w7d4/goal_app/app/controllers/users_controller.rb
@@ -1,25 +1,59 @@
 class UsersController < ApplicationController
     def index
         @users = User.all
+        render :index
     end
 
     def show 
+        @user = User.find_by(id: params[:id])
+        # debugger
+        if @user 
+            render :show 
+        else
+            redirect_to users_url
+        end
+    end
 
+    def new
+        render :new
     end
 
     def create
-
+        @user = User.new(user_params)
+        if @user.save 
+            redirect_to user_url(@user.id)
+        else
+            redirect_to new_user_url 
+        end
     end
 
     def edit 
-        
+        @user = User.find_by(id: params[:id])
+        if @user
+            render :edit
+        else
+            redirect_to users_url
+        end
     end 
 
     def update 
-
+        @user = User.find_by(id: params[:id])
+        if @user
+            @user.username = user_params[:username]
+            @user.password = user_params[:password]
+            @user.save!
+            redirect_to user_url(@user)
+        else
+            redirect_to users_url
+        end
     end
 
     def destroy
 
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:username, :password)
     end
 end
--- w7d4/goal_app/spec/controllers/users_controller_spec.rb
+++ w7d4/goal_app/spec/controllers/users_controller_spec.rb
@@ -1,12 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-    let(:user_params) {
-        user: {
-            username: "haseebthedream",
-            password: "password123"
+    let(:user_params) do 
+        {
+            user: {
+                username: "haseebthedream",
+                password: "password123"
+            }
         }
-    }
+    end
 
     describe "GET #index" do
         it "should render index view template" do 
@@ -16,28 +18,83 @@ RSpec.describe UsersController, type: :controller do
     end
 
     describe "GET #show" do 
-        it "should render show view template" do 
-            get :show
+        user = FactoryBot.create(:user)
+
+        it "should render the correct user's show page" do 
+            get :show, params: {id: user.id}
             expect(response).to render_template("show")
         end
 
-        it "should render the correct user's show page" do 
-            user = FactoryBot.create(:user)
-            get :show
-            expect(response).to redirect_to(user_url(user))
+        it "should redirect to index if user does not exist" do
+            # user = FactoryBot.create(:user)
+            get :show, params: {id: -1}
+            expect(response).to redirect_to(users_url)
         end
     end
 
     describe "GET #new" do
+        it "should render new template" do 
+            get :new 
+            expect(response).to render_template(:new)
+        end
     end
 
     describe "POST #create" do
+        let(:user_params2) do 
+            {
+                user: {
+                    username: "haseebthedream",
+                    password: "password123"
+                }
+            }
+        end
+
+        it "should save a new user to the database" do
+            post :create, params: user_params2
+            expect(User.find_by(username: "haseebthedream")).to_not be_nil
+        end
+
+        context "when there are valid params" do
+            it "should redirect to users show page" do
+                post :create, params: user_params2
+                user = User.find_by(username: "haseebthedream")
+                expect(response).to redirect_to(user_url(user))
+            end
+        end
+
+        context "when there invalid params" do
+            let(:user_params3) do 
+                {
+                    user: {
+                        username: "haseebthedream",
+                        password: ""
+                    }
+                }
+            end
+            it "should redirect to new page" do
+                post :create, params: user_params3
+                expect(response).to redirect_to new_user_url
+            end
+        end
     end
 
     describe "GET #edit" do 
+        user = FactoryBot.create(:user)
+
+        it "should render edit page if user exist" do 
+            get :edit, params: {id: user.id}
+            expect(response).to render_template(:edit)
+        end
+
+        it "should redirect to index page if user does not exist" do
+            get :edit, params: {id: -1}
+            expect(response).to redirect_to users_url 
+        end
     end
 
     describe "PATCH #update" do
+        it "should update the user's credentials"
+        it "should redirect to the user's updated show page"
     end
 
     describe "DELETE #destroy" do
