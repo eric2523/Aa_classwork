5302e33c0a2fb35b84066b10be94e312a99a7fef
eric2523@gmail.com <eric2523@gmail.com>
2020-09-15 16:23:04 -0700

    Finish p02_controller_spec

--- w8d2/rails_lite/lib/controller_base.rb
+++ w8d2/rails_lite/lib/controller_base.rb
@@ -2,31 +2,51 @@ require 'active_support'
 require 'active_support/core_ext'
 require 'erb'
 require_relative './session'
+require 'byebug'
 
 class ControllerBase
-  attr_reader :req, :res, :params
-
+  attr_reader :req, :res
+  
   # Setup the controller
   def initialize(req, res)
+    @req = req
+    @res = res 
+    @already_built_response = false 
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    @res.status = 302
+    @res.location = url
+
+    raise "double render error" if already_built_response?
+    @already_built_response = true
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    @res.content_type = content_type
+    @res.write(content)
+    
+    if already_built_response?
+      raise "double render error"
+    else
+      @already_built_response = true
+    end
   end
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    dir_path = File.dirname(__FILE__)
+    p dir_path
   end
 
   # method exposing a `Session` object
