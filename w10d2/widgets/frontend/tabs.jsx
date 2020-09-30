4fd02b4442ce8a964b7791e16665bffa38273856
eric2523@gmail.com <eric2523@gmail.com>
2020-09-29 19:38:42 -0700

    remove header for li element

--- w10d2/widgets/frontend/tabs.jsx
+++ w10d2/widgets/frontend/tabs.jsx
@@ -1,46 +1,41 @@
 import React from 'react'
 
 class Tabs extends React.Component {
-    constructor(props) {
-        super(props);
-        this.info = props.info;
-        this.state = {currentTab: 0};
-        this.changeTab = this.changeTab.bind(this)
-    }   
+  constructor(props) {
+      super(props);
+      this.info = props.info;
+      this.state = {currentTab: 0};
+      this.changeTab = this.changeTab.bind(this)
+  }   
 
-    changeTab(e) {
-      // event.preventDefault();
-      let li = e.target;
-      const currentTab = li.id;
-      // console.log(li)
-      // console.log(currentTab)
-      this.setState({ currentTab });
-    }
+  changeTab(e) {
+    let li = e.target;
+    const currentTab = li.id;
+    this.setState({ currentTab });
+  }
 
-    render() {
-        return (
-        <div className="tabs">
-            <ul onClick={ this.changeTab }>
-                {
-                  this.info.map((label, idx)=> {
-                  return (
-                    <li key={idx}>
-                      <header>
-                        <h1 id={idx}> {label.title} </h1> 
-                      </header> 
-                    </li> )
-                  })
-                }
-            </ul>
-            <article>
-              {this.info[this.state.currentTab].content}
-            </article>
-        </div>
-        )
-      }
-    }
+  render() {
+    return (
+    <div className="tabs">
+      <ul onClick={ this.changeTab }>
+          {
+            this.info.map((label, idx)=> {
+              return (
+                <li key={idx} id={idx}>
+                  {label.title}
+                </li>
+              );
+            })
+          }
+      </ul>
+
+      <article>
+        {this.info[this.state.currentTab].content}
+      </article>
+    </div>
+    )
+  }
+}
     
-
-
 export default Tabs
 
