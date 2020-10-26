afda3d908756b63cb0d3ae72824643ee7efae78a
eric2523@gmail.com <eric2523@gmail.com>
2020-10-25 20:13:13 -0700

    prepare for heroku

--- mern_twitter/app.js
+++ mern_twitter/app.js
@@ -2,12 +2,25 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const mongoose = require("mongoose");
-const db = require("./config/keys").mongoURI
+const db = require("./config/keys").mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require("body-parser");
+const passport = require('passport');
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("connected to mongoDB"))
-  .catch((error) => console.log(error))
+  .catch((error) => console.log(error));
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+require('./config/passport')(passport);
+
+app.use(passport.initialize());
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
 app.listen(port, () => console.log(`Server is running on port ${port}`));
